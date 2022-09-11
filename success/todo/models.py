54b36ea58ea72b482a5f4fe50845af9abac46ef7
miranguyen162 <85296652+miranguyen162@users.noreply.github.com>
2022-09-11 22:10:09 +0700

    Delete success directory

--- success/todo/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class task_db(models.Model):
-    task_name = models.CharField(max_length = 200)
-    expected_time = models.FloatField()
-    actual_time = models.FloatField()
-
--- success/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name = "home"),
-    path("plan/", views.plan, name = "plan"),
-    path("report/", views.report, name = "report")
-]
